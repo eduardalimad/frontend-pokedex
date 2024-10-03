a04a0cfd062279265316e94e739024f691c8d6ef
Eduarda Lima <me.eduuarda@gmail.com>
2024-10-03 10:41:51 -0400

    :sparkles: feat: criação de estrutura de router da aplicação

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: Home,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About
     }
   ]
 })
