9245ff4cf67cf12a7e801cf0a36ae9a9d190c6fb
Eduarda Lima <me.eduuarda@gmail.com>
2024-10-02 21:39:58 -0400

    setup projeto

---
+++ src/services/config/index.ts
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const http = axios.create({
+  baseURL: 'https://pokeapi.co/api/v2/',
+  headers: {
+    Accept: 'application/json',
+    Content: 'application/json'
+  }
+})
+
+export { http }
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main>
+    <h1>Welcome to my Vue.js app!</h1>
+  </main>
+</template>
