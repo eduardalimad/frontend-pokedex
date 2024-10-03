65d7a244c9e9a72dd663e26c83a77c5ff7e48df8
Eduarda Lima <me.eduuarda@gmail.com>
2024-10-03 12:28:50 -0400

    :recycle: refactor: reestruturar NavBarDefault

--- src/components/NavbarDefault.vue
+++ src/components/NavbarDefault.vue
@@ -1,42 +1,22 @@
 <template>
   <nav class="navbar">
-    <router-link to="/" aria-label="Ir para a Pokedex">
-      Pokedex
-      <IconPokeball :color="colorIconHome" />
-    </router-link>
-    <router-link to="/about">
-      Sobre
-      <IconPokeball :color="colorIconPokedex" />
-    </router-link>
-
+    <div class="navbar-content">
+      <NavBarLink to="/" label="Pokedex" ariaLabel="Ir para a Pokedex" />
+      <NavBarLink to="/about" label="Sobre" ariaLabel="Ir para Sobre" />
+    </div>
+    <SwitchButton />
   </nav>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
-import IconPokeball from '../assets/icons/IconPokeball.vue';
-import { useRoute } from 'vue-router';
+import NavBarLink from './NavbarLink.vue';
+import SwitchButton from './SwitchButton.vue';
 
-const route = useRoute();
-const iconColors = computed(() => {
-  const baseColor = '#111111';
-  const activeColor = '#f10c49';
-
-  return {
-    home: route.path === '/' ? activeColor : baseColor,
-    pokedex: route.path === '/about' ? activeColor : baseColor,
-  };
-});
-
-
-const colorIconHome = computed(() => iconColors.value.home);
-const colorIconPokedex = computed(() => iconColors.value.pokedex);
 
 </script>
 
 
 <style scoped lang="scss">
-$navbar-bg-color: #fbf8f8f2;
 $navbar-shadow: rgba(0, 0, 0, 0.1);
 $link-color: black;
 $link-hover-color: #ff5350;
@@ -44,39 +24,15 @@ $link-active-color: #ff5350;
 
 .navbar {
   display: flex;
+  justify-content: space-between;
   align-items: center;
   padding: 1rem;
-  background-color: $navbar-bg-color;
   box-shadow: 0 2px 5px $navbar-shadow;
   gap: 1rem;
 
-}
-
-.navbar a {
-  cursor: pointer;
-  padding: 10px;
-  color: $link-color;
-  transition: color 0.3s;
-  font-size: 20px;
+ .navbar-content{
   display: flex;
-  align-items: center;
-  gap: 0.5rem;
-
-  &:hover {
-    color: $link-hover-color;
-  }
-
-  &.active {
-    color: $link-active-color;
-    font-weight: 600;
-    transform: scale(0.95);
-    transition: transform 0.1s;
-  }
-}
-
-.router-link-active {
-  color: $link-active-color !important;
-  font-weight: 500;
-
+ }
+ 
 }
 </style>
---
+++ src/components/NavbarLink.vue
@@ -0,0 +1,48 @@
+<template>
+  <router-link :to="to" :aria-label="ariaLabel" :class="{ active: isActive }">
+    {{ label }}
+  </router-link>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+const props = defineProps({
+  to: { type: String, required: true },
+  label: { type: String, required: true },
+  ariaLabel: { type: String, required: true }
+});
+
+const route = useRoute();
+
+const isActive = computed(() => route.path === props.to);
+
+</script>
+
+<style scoped lang="scss">
+$link-color: black;
+$link-hover-color: #ff5350;
+$link-active-color: #ff5350;
+
+a {
+  cursor: pointer;
+  padding: 10px;
+  transition: color 0.3s;
+  font-size: 20px;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  &:hover {
+    color: $link-hover-color;
+  }
+
+  &.active {
+    color: $link-active-color;
+    font-weight: 600;
+    transform: scale(0.95);
+    transition: transform 0.1s;
+  }
+}
+</style>
