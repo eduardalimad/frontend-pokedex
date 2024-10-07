35e02ed4ceb7b2d53e6959793b1247d70458420e
Eduarda Lima <me.eduuarda@gmail.com>
2024-10-06 21:12:13 -0400

    :recycle: refactor: Ajuste na estrutura do cardInfo, ajuste no estilo e na estrtura

--- src/components/CardInfo.vue
+++ src/components/CardInfo.vue
@@ -1,32 +1,35 @@
 <template>
     <div class="pokemon-card" @click="selectCard">
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
-            :alt="name" class="pokemon-image" />
+      <div class="container-img">
+        <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${idNumber}.svg`"
+        :alt="name" class="pokemon-image" />
+      </div>
         <div class="pokemon-info">
-            <h2 class="pokemon-name">{{ name }}</h2>
+            <h2 class="pokemon-name">{{ capitalizeFirstLetter(name) }}</h2>
             <p class="pokemon-level">Nº {{ idNumber }}</p>
         </div>
+        
     </div>
 </template>
 
 <script setup lang="ts">
+import { capitalizeFirstLetter } from '../utils/utils.js';
+
 const props = defineProps({
-    image: { type: String, required: true },
     name: { type: String, required: true },
     idNumber: { type: Number, required: true }
 });
 
 const emit = defineEmits(['select-card']);
 const selectCard = () => {
-    emit('select-card', { image: props.image, name: props.name, id: props.idNumber });
+    emit('select-card', { name: props.name, id: props.idNumber });
 };
 
 </script>
 
 <style scoped lang="scss">
 .pokemon-card {
-    width: 12rem;
-    height: 14rem;
+    height: 16rem;
     border-radius: 12px;
     padding: 16px;
     text-align: center;
@@ -36,7 +39,11 @@ const selectCard = () => {
     backdrop-filter: blur(8px);
     position: relative;
     background: var(--background-card);
-
+    .container-img{
+        background-color: rgba(198, 204, 204, 0.11);
+        padding: 1rem;
+        border-radius: .4rem;
+    }
     &:hover {
         transform: scale(1.05) translateY(-5px);
         box-shadow: 0 6px 10px rgba(0, 0, 0, 0.2);
@@ -46,25 +53,31 @@ const selectCard = () => {
 
 .pokemon-image {
     width: 100px;
-    height: auto;
+    height: 100px;
 }
 
 .pokemon-name {
-    font-size: 1.5em;
-    margin: 8px 0;
+    font-size: 1em;
+    margin: 4px 0;
 }
 
 .pokemon-level {
-    color: #555;
+    color: var(--textGrey)
 }
 
-@media screen and (max-width: 425px) {
+@media screen and (max-width: 584px) {
     .pokemon-card {
-        width: 100%;
+        // width: 100%;
         height: 10rem;
         display: flex;
         justify-content: space-between;
-
+        min-width: 240px;
+        flex-wrap: wrap;
+        .container-img{
+        background-color: transparent ;
+        padding: 0rem;
+        border-radius: .4rem;
+    }
         .pokemon-image {
             width: 110px;
         }
