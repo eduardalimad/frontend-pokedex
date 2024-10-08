e6d65411ea18e21bcaa2180a41f6786ab0e136a9
Eduarda Lima <me.eduuarda@gmail.com>
2024-10-08 02:28:42 -0400

    :bug: fix: Tratativa de erro na ausência de imagens a partir do ID 650

--- src/components/CardInfo.vue
+++ src/components/CardInfo.vue
@@ -1,92 +1,130 @@
 <template>
-    <div class="pokemon-card" @click="selectCard">
-      <div class="container-img">
-        <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${idNumber}.svg`"
-        :alt="name" class="pokemon-image" />
-      </div>
-        <div class="pokemon-info">
-            <h2 class="pokemon-name">{{ capitalizeFirstLetter(name) }}</h2>
-            <p class="pokemon-level">Nº {{ idNumber }}</p>
-        </div>
-        
+  <div class="pokemon-card" @click="selectCard">
+    <div class="container-img">
+      <img
+        :src="currentImage(idNumber)"
+        :alt="name"
+        class="pokemon-image"
+        @error="handleError"
+      />
     </div>
+    <div class="pokemon-info">
+      <h2 class="pokemon-name">{{ capitalizeFirstLetter(name) }}</h2>
+      <p class="pokemon-level">Nº {{ idNumber }}</p>
+      <section class="icon-favorites">
+        <IconFavorites :isFavorited="isFavorite(idNumber)" />
+      </section>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { capitalizeFirstLetter } from '../utils/utils.js';
+import { useFavoriteStore } from "@/stores/useFavoriteStore.js";
+import { capitalizeFirstLetter } from "../utils/utils.js";
+import IconFavorites from "./IconFavorites.vue";
+import { computed, ref } from "vue";
+
+const favoriteStore = useFavoriteStore();
+const { isFavorite } = favoriteStore;
 
 const props = defineProps({
-    name: { type: String, required: true },
-    idNumber: { type: Number, required: true }
+  name: { type: String, required: true },
+  idNumber: { type: Number, required: true },
 });
 
-const emit = defineEmits(['select-card']);
-const selectCard = () => {
-    emit('select-card', { name: props.name, id: props.idNumber });
+const emit = defineEmits(["select-card"]);
+
+const currentImageIndex = ref(0);
+const generateImageUrls = (id: number) => {
+  return [
+    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`,
+    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
+    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/${id}.png`,
+    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${id}.png`,
+  ];
+};
+const currentImage = (id: number) => {
+  const imageUrls = generateImageUrls(id);
+  return imageUrls[currentImageIndex.value];
 };
 
+const handleError = () => {
+  setTimeout(() => {
+    currentImageIndex.value++;
+    if (currentImageIndex.value >= 4) {
+      currentImageIndex.value = 3;
+    }
+  }, 1000);
+};
+const selectCard = () => {
+  emit("select-card", { name: props.name, id: props.idNumber });
+};
 </script>
 
 <style scoped lang="scss">
 .pokemon-card {
-    height: 16rem;
-    border-radius: 12px;
-    padding: 16px;
-    text-align: center;
-    transition: transform 0.3s ease, box-shadow 0.3s ease;
-    cursor: pointer;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    backdrop-filter: blur(8px);
-    position: relative;
-    background: var(--background-card);
-    .container-img{
-        background-color: rgba(198, 204, 204, 0.11);
-        padding: 1rem;
-        border-radius: .4rem;
-    }
-    &:hover {
-        transform: scale(1.05) translateY(-5px);
-        box-shadow: 0 6px 10px rgba(0, 0, 0, 0.2);
-
-    }
+  height: 16.5rem;
+  border-radius: 12px;
+  padding: 16px;
+  text-align: center;
+  transition: transform 0.3s ease, box-shadow 0.3s ease;
+  cursor: pointer;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(8px);
+  position: relative;
+  background: var(--background-card);
+  .container-img {
+    background-color: rgba(198, 204, 204, 0.11);
+    padding: 1rem;
+    border-radius: 0.4rem;
+  }
+  &:hover {
+    transform: scale(1.05) translateY(-5px);
+    box-shadow: 0 6px 10px rgba(0, 0, 0, 0.2);
+  }
 }
-
+.icon-favorites {
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  gap: 2px;
+}
 .pokemon-image {
-    width: 100px;
-    height: 100px;
+  width: 100px;
+  height: 100px;
 }
 
 .pokemon-name {
-    font-size: 1em;
-    margin: 4px 0;
+  font-size: 1em;
+  margin: 4px 0;
 }
 
 .pokemon-level {
-    color: var(--textGrey)
+  color: var(--textGrey);
 }
 
 @media screen and (max-width: 584px) {
-    .pokemon-card {
-        height: 10rem;
-        display: flex;
-        justify-content: space-between;
-        min-width: 240px;
-        flex-wrap: wrap;
-        .container-img{
-        background-color: transparent ;
-        padding: 0rem;
-        border-radius: .4rem;
+  .pokemon-card {
+    height: 10rem;
+    display: flex;
+    justify-content: space-between;
+    min-width: 240px;
+    flex-wrap: wrap;
+    .container-img {
+      background-color: transparent;
+      padding: 0rem;
+      border-radius: 0.4rem;
     }
-        .pokemon-image {
-            width: 110px;
-        }
+    .pokemon-image {
+      width: 110px;
+    }
 
-        .pokemon-info {
-            display: flex;
-            flex-direction: column;
-            justify-content: flex-start;
-            align-items: flex-end;
-        }
+    .pokemon-info {
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: flex-end;
     }
+  }
 }
 </style>
--- src/components/PokemonEvolutions.vue
+++ src/components/PokemonEvolutions.vue
@@ -2,8 +2,16 @@
   <div class="evolution-list">
     <div v-if="isLoading" class="loading">Carregando evoluções...</div>
     <div v-else>
-      <div v-for="(evolution, index) in processedEvolutions" :key="index" class="evolution-item">
-        <img :src="evolution.image" :alt="evolution.species" class="image" />
+      <div
+        v-for="(evolution, index) in processedEvolutions"
+        :key="index"
+        class="evolution-item"
+      >
+        <img
+        :src="currentImage(evolution.id)"
+        class="pokemon-image"
+        @error="handleError"
+      />
         <section class="evolution-info-pokemon">
           <span>{{ evolution.species }}</span>
           <PokemonTypeList :types="typeData" :getColor="getColor" />
@@ -14,99 +22,123 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watch, type PropType } from 'vue';
-import PokemonTypeList from './PokemonTypeList.vue';
-import { getIdFromUrl } from '../utils/utils';
-import type { ProcessedEvolution } from '@/types/PokemonEvolution';
+import { ref, watch, type PropType } from "vue";
+import PokemonTypeList from "./PokemonTypeList.vue";
+import { getIdFromUrl } from "../utils/utils";
+import type { ProcessedEvolution } from "@/types/PokemonEvolution";
 
 // Tipos personalizados
-type TypeData = { name: string; url: string }; 
-type Colors = Record<string, string>; 
+type TypeData = { name: string; url: string };
+type Colors = Record<string, string>;
 
 const props = defineProps({
   evolutionsData: {
-    type: Array as PropType<ProcessedEvolution[]>, 
+    type: Array as PropType<ProcessedEvolution[]>,
     required: true,
   },
   typeData: {
-    type: Array as PropType<TypeData[]>, 
+    type: Array as PropType<TypeData[]>,
     required: true,
   },
 });
 const isLoading = ref(true);
 const processedEvolutions = ref<ProcessedEvolution[]>([]);
-const colors = ref<Colors>({ 
-  fire: '#FDDFDF',
-  grass: '#DEFDE0',
-  electric: '#FCF7DE',
-  water: '#DEF3FD',
-  ground: '#f4e7da',
-  rock: '#d5d5d4',
-  fairy: '#fceaff',
-  poison: '#b97fc9',
-  bug: '#f8d5a3',
-  dragon: '#97b3e6',
-  psychic: '#eaeda1',
-  flying: 'rgba(144, 167, 218, 1)',
-  fighting: '#E6E0D4',
-  normal: '#F5F5F5',
+const colors = ref<Colors>({
+  fire: "#fd7d24",
+  grass: "#DEFDE0",
+  electric: "#eed535",
+  water: "#4592c4",
+  ground: "#f7de3f",
+  rock: "#a38c21",
+  fairy: "#fceaff",
+  poison: "#b97fc9",
+  bug: "#729f3f",
+  dragon: "#97b3e6",
+  psychic: "#f366b9",
+  flying: "rgba(144, 167, 218, 1)",
+  fighting: "#E6E0D4",
+  normal: "#F5F5F5",
+  ghost: "#7b62a3",
 });
 
-const getColor = (typeName :string) :string => {
-  return colors.value[typeName] || '#000';
-
+const getColor = (typeName: string): string => {
+  return colors.value[typeName] || "#000";
 };
 
-watch(() => props.evolutionsData, (newEvolutions) => {
-  if (newEvolutions && newEvolutions.length) {
-    isLoading.value = true;
+const currentImageIndex = ref(0);
+const generateImageUrls = (id:number) => {
+  return [
+    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`,
+    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
+    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/${id}.png`,
+    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${id}.png`,
+  ];
+};
+const currentImage = (id: number) => {
+  const imageUrls = generateImageUrls(id);
+  return imageUrls[currentImageIndex.value];
+};
 
-    setTimeout(() => {
-      processedEvolutions.value = newEvolutions.map((evolution: ProcessedEvolution) => {
-        const url = evolution.url; 
-        const id = getIdFromUrl(url ?? '');
+const handleError = () => {
+  setTimeout(() => {
+    currentImageIndex.value++;
+    if (currentImageIndex.value >= 4) {
+      currentImageIndex.value = 3;
+    }
+  }, 1000);
+};
 
-        return {
-          species: evolution.species,
-          id,
-          image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`
-        };
-      });
-      isLoading.value = false;
-    }, 300);
-  }
-}, { immediate: true });
+watch(
+  () => props.evolutionsData,
+  (newEvolutions) => {
+    if (newEvolutions && newEvolutions.length) {
+      isLoading.value = true;
 
+      setTimeout(() => {
+        processedEvolutions.value = newEvolutions.map(
+          (evolution: ProcessedEvolution) => {
+            const url = evolution.url;
+            const id = getIdFromUrl(url ?? "");
+
+            return {
+              species: evolution.species,
+              id,
+              image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`,
+            };
+          }
+        );
+        isLoading.value = false;
+      }, 300);
+    }
+  },
+  { immediate: true }
+);
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
+.evolution-list {
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
 
-  .evolution-list {
+  .evolution-item {
     display: flex;
-    flex-direction: column;
-    padding: 1rem;
+    align-items: center;
+    margin: 5px 0;
 
-
-    .evolution-item {
-      display: flex;
-      align-items: center;
-      margin: 5px 0;
-
-      img {
-        width: 5rem;
-        height: 5rem;
-        margin-right: 10px;
-      }
-      .evolution-info-pokemon{
-        display: grid;
-      }
+    img {
+      width: 5rem;
+      height: 5rem;
+      margin-right: 10px;
     }
-    
+    .evolution-info-pokemon {
+      display: grid;
+    }
   }
-  @media screen and (max-width: 584px) {
-    .evolution-list {
-      margin-bottom: 7rem;
-    
+}
+@media screen and (max-width: 584px) {
+  .evolution-list {
+    margin-bottom: 7rem;
   }
 }
 </style>
