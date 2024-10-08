8acf8c4dcb8097510d98142dd4ee6d82467b2ef8
Eduarda Lima <me.eduuarda@gmail.com>
2024-10-08 02:37:44 -0400

    :recycle: refactor: Alteração na estrutura de rotas e adição de rotas para favoritos

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-import About from '../views/About.vue'
+import Favorites from '../views/Favorites.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -10,9 +10,9 @@ const router = createRouter({
       component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
-      component: About
+      path: '/favorites',
+      name: 'favorites',
+      component: Favorites
     }
   ]
 })
